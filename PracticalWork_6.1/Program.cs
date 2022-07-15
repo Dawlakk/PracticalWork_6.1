28666487e2fbada3483f3d0c76454de101c3c330
Asus <Asus@LAPTOP-V605S04R>
2022-07-15 19:38:11 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork_6.1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PracticalWork_6._1
+{
+    internal class Program
+    {
+        static void Checking()
+        {
+            bool z = File.Exists("directory.txt");
+            if (z == false)
+            {
+                File.WriteAllText("directory.txt", "Список сотрудников\n", Encoding.Unicode);
+            }
+        }
+        static void Output()
+        {
+            string[] lines = File.ReadAllLines("directory.txt");
+            foreach (var line in lines) Console.WriteLine(line.Replace("#", "   "));
+        }
+        static void Filling(int count)
+        {
+
+            using (StreamWriter sw = new StreamWriter("directory.txt", true, Encoding.Unicode))
+            {
+                string note = string.Empty;
+                Console.WriteLine($"Порядковый номер записи: {count}");
+                note = $"{count}#";
+
+                string now = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+                Console.WriteLine($"Дата и время записи {now}");
+                note += $"{now}#";
+
+                Console.Write("Введите Ф.И.О. сотрудника: ");
+                note += $"{Console.ReadLine()}#";
+
+                Console.Write("Введите возраст: ");
+                note += $"{Console.ReadLine()}#";
+
+                Console.Write("Введите рост сотрудника: ");
+                note += $"{Console.ReadLine()}#";
+
+                Console.Write("Введите дату рождения: ");
+                note += $"{Console.ReadLine()}#";
+
+                Console.Write("Введите место рождения: ");
+                note += $"{Console.ReadLine()}";
+                sw.WriteLine(note);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Checking();
+            bool x = true;
+            while (x == true)
+            {
+                int count = File.ReadAllLines("directory.txt").Length;
+                Console.WriteLine($"\nНажмите 1, если хотите просмотреть содержимое справочника и выйти из программы.\nНажмите 2, если хотите добавить новую запись в справочник.");
+                string n = Console.ReadLine();
+                if (n == "1") { Output(); x = false; }
+                else if (n == "2") Filling(count);
+                else Console.WriteLine("Некорректный ввод, попробуйте снова");
+            }
+            Console.ReadLine();
+        }
+    }
+}
